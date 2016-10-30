544d3a944ff0d6b6683a42f785e5423b6edabc43
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-10-30 13:13:57 -0200

    aula 1 e 2 do curso do Alura

---
+++ ocjp7/src/main/java/br/abevieiramota/ocjp7/Aula1.java
@@ -0,0 +1,32 @@
+package main.java.br.abevieiramota.ocjp7;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+// n pode static e instance c msm método
+@RunWith(JUnit4.class)
+public class Aula1 {
+	String nome = "tsc";
+	
+	static String nome2;
+
+	@Test
+	public void test() {
+		String nome = "oi";
+		
+		assertEquals("oi", nome);
+		assertEquals("tsc", this.nome);
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setIdade(int nome) {
+		this.nome = Integer.toString(nome, 2);
+	}
+
+}
