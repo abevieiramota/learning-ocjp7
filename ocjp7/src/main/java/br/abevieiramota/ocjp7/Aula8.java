d03efbeeeb6fc832590ce44d708102653217d2a8
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-11-24 15:08:36 -0200

    aula 8

---
+++ ocjp7/src/main/java/br/abevieiramota/ocjp7/Aula8.java
@@ -0,0 +1,16 @@
+package br.abevieiramota.ocjp7;
+
+public class Aula8 {
+
+	public static void main(String[] args) {
+		// atribuição, direito deve ser menos específico que o da esquerda
+		// byte, short, int, long
+		// char tem mesmo tamanho de short, mas só pode positivo
+		// float, double
+		// todos inteiros cabem em float
+		// se valor fora dos limites do tipo, não compila!
+		// char pode converter para int e long
+		char x = 100;
+//		short b = x;
+	}
+}
