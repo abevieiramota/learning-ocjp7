26379b54189e2bdbb8e872a55c25c4122268d53c
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-11-25 16:15:02 -0200

    aula 9

---
+++ ocjp7/src/main/java/br/abevieiramota/ocjp7/Aula9.java
@@ -0,0 +1,18 @@
+package br.abevieiramota.ocjp7;
+
+public class Aula9 {
+
+	public static void main(String[] args) {
+
+		// int operador int = int
+		// int operador long = long
+		// O MÍNIMO QUE UMA OPERAÇÃO É INT!!!!!!!!!!!!!!!!!!!!!!!!!!
+		byte a = 10;
+		byte b = 20;
+//		byte c = a + b; <<<<<<<<<<<<<<<<<
+		// só pode atribuir para tipo igual ou mais amplo
+		// int / 0 -> ArithmeticException
+		// float ou double / 0 -> existe!!!!!!!!!!!!!!!!!!!!!!!! infinito!
+		// +inf + -inf = NaN
+	}
+}
